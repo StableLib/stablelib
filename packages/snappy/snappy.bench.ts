e3ba1f73893c0fd50a0fbe695cae457e6b4a44f8
Dmitry Chestnykh <dmitry@codingrobots.com>
2017-10-26 14:47:33 +0200

    snappy: add zlib level 1 benchmark for comparison
    MacBook Pro (Retina, 13-inch, Mid 2014) 2.6 GHz Intel Core i5
    Node.js v8.6.0
    
    Snappy compress                             823 ops           0.61 ms/op      1645.91 ops/sec     73.12 MiB/s
    Snappy decompress                          1642 ops           0.31 ms/op      3275.08 ops/sec    145.50 MiB/s
    zlib-1 compress                             556 ops           0.90 ms/op      1107.89 ops/sec     49.22 MiB/s
    zlib-1 decompress                          1394 ops           0.36 ms/op      2784.06 ops/sec    123.69 MiB/s
    Snappy compressed length: 31352
    zlib-1 compressed length: 19530

--- packages/snappy/snappy.bench.ts
+++ packages/snappy/snappy.bench.ts
@@ -4,7 +4,9 @@
 import { benchmark, report } from "@stablelib/benchmark";
 import { compress, maxCompressedLength } from "./compress";
 import { decompress } from "./decompress";
+import * as zlib from "zlib";
 import data from "./compress.test-data";
+
 
 const compressedData = compress(data);
 const decompressedData = new Uint8Array(data.length);
@@ -14,3 +16,13 @@ const compressedDst = new Uint8Array(maxCompressedLength(data.length));
 
 report("Snappy compress", benchmark(() => compress(data, compressedDst), data.length));
 report("Snappy decompress", benchmark(() => decompress(compressedData, decompressedData), data.length));
+
+const zData = Buffer.from(data);
+const zCompressedData = zlib.deflateSync(zData);
+const zOpts = { level: zlib.constants.Z_BEST_SPEED };
+
+report("zlib-1 compress", benchmark(() => zlib.deflateSync(zData, zOpts), zData.length));
+report("zlib-1 decompress", benchmark(() => zlib.inflateSync(zCompressedData), zData.length));
+
+console.log("Snappy compressed length:", compressedData.length);
+console.log("zlib-1 compressed length:", zCompressedData.length);
