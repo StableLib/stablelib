94ac5c7ed6bd52b773beef653f93ac65dce48b0f
Dmitry Chestnykh <dmitry@codingrobots.com>
2024-06-29 17:14:03 +0200

    Remove shims from int

--- packages/int/int.ts
+++ packages/int/int.ts
@@ -5,16 +5,8 @@
  * Package int provides helper functions for integerss.
  */
 
-// Shim using 16-bit pieces.
-function imulShim(a: number, b: number): number {
-    const ah = (a >>> 16) & 0xffff, al = a & 0xffff;
-    const bh = (b >>> 16) & 0xffff, bl = b & 0xffff;
-    return ((al * bl) + (((ah * bl + al * bh) << 16) >>> 0) | 0);
-}
-
 /** 32-bit integer multiplication.  */
-// Use system Math.imul if available, otherwise use our shim.
-export const mul = (Math as { imul?(a: number, b: number): number }).imul || imulShim;
+export const mul = Math.imul;
 
 /** 32-bit integer addition.  */
 export function add(a: number, b: number): number {
@@ -36,29 +28,18 @@ export function rotr(x: number, n: number): number {
     return x << (32 - n) | x >>> n;
 }
 
-function isIntegerShim(n: number): boolean {
-    return typeof n === "number" && isFinite(n) && Math.floor(n) === n;
-}
-
 /**
  * Returns true if the argument is an integer number.
- *
- * In ES2015, Number.isInteger.
  */
-export const isInteger = (Number as { isInteger?(n: number): boolean }).isInteger || isIntegerShim;
+export const isInteger = Number.isInteger;
 
 /**
  *  Math.pow(2, 53) - 1
- *
- *  In ES2015 Number.MAX_SAFE_INTEGER.
  */
-export const MAX_SAFE_INTEGER = 9007199254740991;
+export const MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER;
 
 /**
  * Returns true if the argument is a safe integer number
  * (-MIN_SAFE_INTEGER < number <= MAX_SAFE_INTEGER)
- *
- * In ES2015, Number.isSafeInteger.
  */
-export const isSafeInteger = (n: number): boolean =>
-    isInteger(n) && (n >= -MAX_SAFE_INTEGER && n <= MAX_SAFE_INTEGER);
+export const isSafeInteger = Number.isSafeInteger;
