0cc1fd941fe00f74df1d6ff5366cea51f2b7e503
Dmitry Chestnykh <dmitry@codingrobots.com>
2024-06-29 17:25:54 +0200

    float: remove shims

--- packages/float/float.ts
+++ packages/float/float.ts
@@ -5,46 +5,21 @@
  * Package float provides helper functions for floating-point numbers.
  */
 
-interface ObjectWithIs extends Object {
-    is?: (a: any, b: any) => boolean;
-}
-
 /**
  * Returns true if the argument is -0, false otherwise.
  */
-export const isNegativeZero =
-    typeof (Object as ObjectWithIs).is !== "undefined"
-        ? (x: number) => (Object as ObjectWithIs).is!(x, -0)
-        : (x: number) => x === 0 && (1 / x < 0);
-
-
-const froundShim = (() => {
-    const tmp = new Float32Array(1);
-    return (x: number) => {
-        tmp[0] = +x;
-        return tmp[0];
-    };
-})();
+export const isNegativeZero = (x: number) => Object.is(x, -0)
 
 /**
  * Returns new number rounded to float32.
- *
- *  In ES2015, Math.fround.
  */
-export const fround = ((Math as { fround?: (x: number) => number })).fround || froundShim;
-
-
-function log2Shim(x: number) {
-    return Math.log(x) * Math.LOG2E;
-}
+export const fround = Math.fround;
 
 /**
  * Returns base 2 logarithm.
  *
  * Note that result approximation is implementation-dependent.
  * If result is to be used in bit masks, use Math.round() on it.
- *
- * In ES2015, Math.log2.
  */
-export const log2 = ((Math as { log2?: (x: number) => number})).log2 || log2Shim;
+export const log2 = Math.log2;
 
